49c8b73e4d4714368fe59d8eb2ea8d2af670d1a7
issmuth <ismael.mdev@gmail.com>
2024-09-27 15:22:44 +0300

    fixes

--- peakcraft-landing/next.config.mjs
+++ peakcraft-landing/next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    basePath:'/Peak-Craft',
-    assetPrefix: 'https://peak-craft.github.io/Peak-Craft',
-};
+const nextConfig = {};
 
 export default nextConfig;
